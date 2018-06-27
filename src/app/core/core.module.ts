2a5d4a237d9db91c6859105038ca49113a6ad3f0
Kwaku Prempeh <kwaku.prempeh@holition.com>
2018-06-27 17:19:01 +0100

    clean up

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,7 +4,6 @@ import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
-
 @NgModule({
   imports: [
     CommonModule,
